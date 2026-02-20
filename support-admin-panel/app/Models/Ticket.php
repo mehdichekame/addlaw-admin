903043b0b47e079dd0fcecd079a8371206a2dc1a
MehdiCh <mehdi.chekame@gmail.com>
2026-02-20 04:31:01 +0330

    yekta pay

--- support-admin-panel/app/Models/Ticket.php
+++ support-admin-panel/app/Models/Ticket.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Ticket extends Model
 {
@@ -41,4 +43,17 @@ class Ticket extends Model
 //    {
 //        return $this->belongsTo(User::class);
 //    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+// اگر می‌خوای کاربر تیکت هم مشخص باشه:
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
 }
